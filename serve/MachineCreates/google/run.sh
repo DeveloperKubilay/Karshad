7161b2c5c13b3fd582c5039b9aea3df15b7e4672
Developerkubilay <kubiss52@yahoo.com>
2025-09-05 21:08:49 +0300

    fix: Remove Express server setup and switch to Python HTTP server

--- serve/MachineCreates/google/run.sh
+++ serve/MachineCreates/google/run.sh
@@ -9,18 +9,6 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 
 curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
 sudo apt-get install -y nodejs
-npm i express
 
 cd $HOME
-echo 'const express = require("express"),
-      app = express(),
-      port = 80;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, "0.0.0.0");' > index.js
-nohup sudo node index.js > /dev/null 2>&1 &
-
-#curl http://localhost
+nohup sudo python3 -m http.server 80 > /dev/null 2>&1 &
