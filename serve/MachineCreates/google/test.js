3d47cc9bfbb92f329dec8f2e501048fa5b193fda
Developerkubilay <kubiss52@yahoo.com>
2025-09-05 21:42:59 +0300

    feat: Add Google Cloud Compute instance management functions and speed test script

---
+++ serve/MachineCreates/google/test.js
@@ -0,0 +1,48 @@
+const { InstancesClient } = require('@google-cloud/compute');
+const projectId = "dotted-lens-470017-f5";
+const zone = "us-central1-b";
+const google = new InstancesClient({ keyFilename: "./machine/env.json" });
+
+async function getInstanceStatus(instanceName) {
+
+    const [instance] = await google.get({
+        project: projectId,
+        zone: zone,
+        instance: instanceName,
+    });
+    return instance.status;
+}
+
+async function setInstanceStatus(instanceName, start) {
+
+    if (start) {
+        await google.start({
+            project: projectId,
+            zone: zone,
+            instance: instanceName,
+        });
+        console.log(`VM ${instanceName} başlatıldı`);
+    } else {
+        await google.stop({
+            project: projectId,
+            zone: zone,
+            instance: instanceName,
+        });
+        console.log(`VM ${instanceName} durduruldu`);
+    }
+}
+
+
+async function main() {
+    const instanceName = "instance-1757095267949";
+
+    let status = await getInstanceStatus(instanceName);
+    console.log(`Instance ${instanceName} durumu: ${status}`);
+
+    await setInstanceStatus(instanceName, true);
+
+    status = await getInstanceStatus(instanceName);
+    console.log(`Instance ${instanceName} durumu: ${status}`);
+}
+
+main()
---
+++ serve/dev/test.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install speedtest-cli -y
+speedtest-cli
